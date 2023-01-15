ba910164877635c7f2a3be208e13f8ed85f0d137
P-Horus <feranmia51@gmail.com>
2023-01-15 23:47:44 +0100

    Add Profile Update

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 # CASCADE MEANS THAT IF THE USER IS DELETED, THEN DELELTE THE USER, BUT IF THE PROFILE IS DELETED THEN IT DOESN'T DELTE THE USER
@@ -10,3 +11,12 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'{self.user.username} Profile' 
+    
+    def save(self):
+        super().save()
+        
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
